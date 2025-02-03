c3d942413c78057e9f39435e2d558839bed34b4d
Tyler Chauvy <113985981+ty-34@users.noreply.github.com>
2025-02-03 15:42:50 +0000

    added doc string function and fixed pylint issues
    mind that the indetentions work properly as they have been changed

--- Project1/ver3/Task1ver3.py
+++ Project1/ver3/Task1ver3.py
@@ -1,7 +1,7 @@
 #!/bin/python3
 
-import numpy as np
 import time
+import numpy as np
 from mpi4py import MPI
 
 # MPI.Init()
@@ -10,9 +10,9 @@ comm = MPI.COMM_WORLD
 # MPI-related data
 rank = comm.Get_rank()
 size = comm.Get_size()
-nworkers = size # processor rank 0 is leader, the rest are workers
+nworkers = size  # processor rank 0 is leader, the rest are workers
 
-# Set up initial message 
+# Set up initial message
 sum1 = np.array(0, dtype=np.float64)
 
 # Integral setup
@@ -22,26 +22,45 @@ sum1 = np.array(0, dtype=np.float64)
 num_points = int(10000000/nworkers)
 dx = (nworkers*(num_points-1))**(-1)
 
-# Integration function
-def integrand(x):
-	return (4.0 / (1.0 + x**2))
+
+def integrand(x_point):
+    """
+    Function we which to integrate
+
+    Parameters
+    ----------
+    x :float
+        Point at which we evaluate the function at.
+
+    Returns
+    -------
+    float
+        f(x)=4.0/(1+x^2).
+
+    """
+    return 4.0 / (1.0 + x_point**2)
+
 
 timeStart = time.time()
-if rank!=0: # Workers calculate and send their own "chunk" 
-	for x_var in np.linspace(rank/nworkers, (rank+1)/nworkers, num_points)[:-1]:
-		sum1 += integrand(x_var+1/2*dx) * dx
-	request = comm.Isend(sum1, dest=0)
-	request.wait()
-else: # Leader rank 0 calculates its own "chunk" and receives data from workers
-	final_sum = 0
-	for x_var in np.linspace(rank/nworkers, (rank+1)/nworkers, num_points)[:-1]:
-		final_sum += integrand(x_var+1/2*dx) * dx
-	for i in range (1, nworkers):
-		comm.Recv(sum1, source=i)
-		final_sum += sum1
-	# The leader rank is the last rank to process the data
-	timeEnd = time.time()
-	timeTaken = timeEnd-timeStart
-		
-	print("The final calculation of pi for", nworkers, "workers is equal to", final_sum)
-	print("Total time taken:", timeTaken, "s")
+if rank != 0:  # Workers calculate and send their own "chunk"
+    for x_var in np.linspace(rank/nworkers, (rank+1)/nworkers,
+                             num_points)[:-1]:
+        sum1 += integrand(x_var+1/2*dx) * dx
+    request = comm.Isend(sum1, dest=0)
+    request.wait()
+else:  # Leader rank 0 calculates its own "chunk" and receives
+    # data from workers
+    FINALSUM = 0
+    for x_var in np.linspace(rank/nworkers, (rank+1)/nworkers,
+                             num_points)[:-1]:
+        FINALSUM += integrand(x_var+1/2*dx) * dx
+    for i in range(1, nworkers):
+        comm.Recv(sum1, source=i)
+        FINALSUM += sum1
+    # The leader rank is the last rank to process the data
+    timeEnd = time.time()
+    timeTaken = timeEnd-timeStart
+
+    print("The final calculation of pi for", nworkers, "workers is equal to",
+          FINALSUM)
+    print("Total time taken:", timeTaken, "s")
