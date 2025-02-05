4a1616fee31f62f58f751a58d6383d549df188fc
Tyler Chauvy <113985981+ty-34@users.noreply.github.com>
2025-02-05 22:17:25 +0000

    Update speed_up_plot.py
    made it pretty

--- Project1/ver4/speed_up_plot.py
+++ Project1/ver4/speed_up_plot.py
@@ -1,11 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-N = np.array([2, 4, 8, 16])
-
+N = np.array([1, 2, 4, 8, 16])
+N = np.array(["1", "2", "4", "8", "16"])
 T_1 = 117.065
 
-T_N = np.array([117.838, 58.528, 40.711, 23.427])
+T_N = np.array([117.065, 117.838, 58.528, 40.711, 23.427])
 
 S_N = T_1/T_N
 
@@ -14,3 +14,4 @@ print(S_N)
 plt.plot(N, np.array(S_N))
 plt.xlabel("Number of processors")
 plt.ylabel("Speedup")
+plt.xlim([0,4])
