4d8f53f1ecfa467d4a80249d7c8a6590131b3c12
lailasfv <sohaila.safavi.2019@uni.strath.ac.uk>
2025-02-10 15:45:49 +0000

    Added docstrings, comments and angle function

--- Project2/Tasks_1_2_3a.py
+++ Project2/Tasks_1_2_3a.py
@@ -1,31 +1,52 @@
 import numpy as np
 
 # Task 1
-# ADD THAT WE SPECIFY THAT STUFF IS RADIANTS CAUSE WE ARE FANCY
+# ADD THAT WE SPECIFY THAT STUFF IS RADIANS CAUSE WE ARE FANCY
 
 class Vector:
+    """
+    Vector class for three dimensional quantities in cartesian coordinates
+    """
     def __init__(self, x, y, z):
         self.x = x
         self.y = y
         self.z = z
 
     def __str__(self):
-        return f"({self.x},{self.y},{self.z})" # REWRITE THAT WITH THE EXAMPLE
+        """
+        Assumes floating point when printing
+        """
+        return f"({self.x:6f},{self.y:6f},{self.z:6f})" # REWRITE THAT WITH THE EXAMPLE
 
     def norm(self):
+        """
+        Returns magnitude of given instance
+        """
         return np.sqrt(self.x**2+self.y**2+self.z**2)
 
     def __add__(self, other):
+        """
+        Returns addition of two instance vectors as instance vector
+        """
         return Vector(self.x+other.x, self.y+other.y, self.z+other.z)
 
     def __sub__(self, other):
+        """
+        Returns subtraction of two instance vectors as instance vector
+        """
         return Vector(self.x-other.x, self.y-other.y, self.z-other.z)
 
     def __dot__(self, other):
+        """
+        Returns scalar product of two instance vectors as value
+        """
         return self.x*other.x+self.y*other.y+self.z*other.z
         # Vector.__dot__(v1,v2)
 
     def __cross__(self, other):
+        """
+        Returns vector product of two instance vectors as instance vector
+        """
         return Vector((self.y*other.z-self.z*other.y),
                       (self.z*other.x-self.x*other.z),
                       (self.x*other.y-self.y*other.x))
@@ -34,9 +55,15 @@ class Vector:
 
 
 class Vector_spherical(Vector):
+    """
+    Vector class for three dimensional quantities in spherical coordinates.
+    Input instance for theta and phi is taken to be in RADIAN units 
+    """
     def __init__(self, rho, theta, phi):
-        self.x = rho*np.sin(phi)*np.cos(theta)  # this is in radiants
-        # DO WE NEED TO USE DEGREES?
+        """
+        Initialises instance with cartesian vector components
+        """
+        self.x = rho*np.sin(phi)*np.cos(theta)  # this is in radians
         self.y = rho*np.sin(phi)*np.sin(theta)
         self.z = rho*np.cos(phi)
 
@@ -61,21 +88,44 @@ def area_triangle(v1, v2):
     # WE COULD PUT THAT IN THE CLASS YEAH
     return (Vector.__cross__(v1, v2)).norm()/2
 
+def internal_angle(v1, v2):
+    # a.b/|a||b|
+	return np.arccos(Vector.__dot__(v1, v2))/(Vector.norm(v1)*Vector.norm(v2))
 
-v1_a_cart = Vector(1, 0, 0)
-v1_b_cart = Vector(0, 1, 0)
+
+
+# REMOVE THIS AFTER
+# v1a = (0, 0, 0)
+# v1b = (1, 0, 0)
+# v1c = (0, 1, 0)
+# What if we make class do this? Gives vector for given coordinates?
+v1_a_cart = Vector(1-0, 0-0, 0-0)
+v1_b_cart = Vector(0-0, 1-0, 0-0)
+v1_c_cart = Vector(0-1, 1-0, 0-0)
 # area_1_cart = (Vector.__cross__(v1_a_cart,v1_b_cart)).norm()/2
 area_1_cart = area_triangle(v1_a_cart, v1_b_cart)
+angle_1ab_cart = internal_angle(v1_a_cart, v1_b_cart)
 
+
+# REMOVE THIS AFTER
+# v2a = (-1, -1, -1)
+# v2b = (0, -1, -1)
+# v2c = (-1, 0, -1)
 v2_a_cart = Vector(-1, -1+1, -1+1)
 v2_b_cart = Vector(-1+1, -1, -1+1)
+v2_c_cart = vector(-1-0, 0+1, -1+1)
 area_2_cart = area_triangle(v2_a_cart, v2_b_cart)
+angle_2ab_cart = internal_angle(v2_a_cart, v2_b_cart)
 
 v3_a_cart = Vector(1, 0, -1)
 v3_b_cart = Vector(1, 0, 0)
 area_3_cart = area_triangle(v3_a_cart, v3_b_cart)
+angle_3ab_cart = internal_angle(v3_a_cart, v3_b_cart)
 
 v4_a_cart = Vector(-1, 1, 0)
 v4_b_cart = Vector(0, 0, -1)
 area_4_cart = area_triangle(v4_a_cart, v4_b_cart)
-# I DO SEEM TO HAVE ISSUE WITH CALCULATION SLIGHTLY OFF FOR EVERYTHING??
+angle_4ab_cart = internal_angle(v4_a_cart, v4_b_cart)
+angle_list = np.array([angle_1_cart, angle_2_cart, angle_3_cart, angle_4_cart])
+angle_degrees = angle_list*180/np.pi
+print(angle_degrees)
