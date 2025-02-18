27ca65242fdebfeb7cda6611fa188c9f3f0ee090
Tyler Chauvy <113985981+ty-34@users.noreply.github.com>
2025-02-18 13:21:02 +0000

    Update tasks.py
    minor pep8 fixes

--- Project2/tasks.py
+++ Project2/tasks.py
@@ -5,7 +5,7 @@ This does not need a module docstring"""
 import numpy as np
 from vector3d import Vector, VectorSpherical
 
-#------------------------------
+# ------------------------------
 # TRIANGLE 1 - Cartesian
 
 v1a = Vector(0, 0, 0)
@@ -18,7 +18,7 @@ print(f"Triangle 1 area (cartesian) :{area_1_cart}")
 angle_1_cart = Vector.angle_vertices(v1a, v1b, v1c)
 print(f"Triangle 1 angles (cartesian):{angle_1_cart*180/np.pi}")
 
-#------------------------------
+# ------------------------------
 # TRIANGLE 2 - Cartesian
 
 v2a = Vector(-1.0, -1.0, -1.0)
@@ -31,7 +31,7 @@ print(f"Triangle 2 area (cartesian) :{area_2_cart}")
 angle_2_cart = Vector.angle_vertices(v2a, v2b, v2c)
 print(f"Triangle 2 angles (cartesian):{angle_2_cart*180/np.pi}")
 
-#------------------------------
+# ------------------------------
 # TRIANGLE 3 - Cartesian
 
 v3a = Vector(1.0, 0, 0)
@@ -45,7 +45,7 @@ angle_3_cart = Vector.angle_vertices(v3a, v3b, v3c)
 print(f"Triangle 3 angles (cartesian):{angle_3_cart*180/np.pi}")
 
 
-#------------------------------
+# ------------------------------
 # TRIANGLE 4 - Cartesian
 
 v4a = Vector(0, 0, 0)
@@ -58,7 +58,7 @@ print(f"Triangle 4 area (cartesian) :{area_4_cart}")
 angle_4_cart = Vector.angle_vertices(v4a, v4b, v4c)
 print(f"Triangle 4 angles (cartesian):{angle_4_cart*180/np.pi}")
 
-#------------------------------
+# ------------------------------
 # TRIANGLE 1 - Spherical
 
 v1a_sph = VectorSpherical(0, 0, 0)
@@ -71,7 +71,7 @@ print(f"Triangle 1 area (spherical) :{area_1_sph}")
 angle_1_sph = VectorSpherical.angle_vertices(v1a_sph, v1b_sph, v1c_sph)
 print(f"Triangle 1 angles (spherical):{angle_1_sph*180/np.pi}")
 
-#------------------------------
+# ------------------------------
 # TRIANGLE 2 - Spherical
 
 v2a_sph = VectorSpherical(1.0, 0, 0)
@@ -84,7 +84,7 @@ print(f"Triangle 2 area (spherical) :{area_2_sph}")
 angle_2_sph = VectorSpherical.angle_vertices(v2a_sph, v2b_sph, v2c_sph)
 print(f"Triangle 2 angles (spherical):{angle_2_sph*180/np.pi}")
 
-#------------------------------
+# ------------------------------
 # TRIANGLE 3 - Spherical
 
 v3a_sph = VectorSpherical(0, 0, 0)
@@ -97,7 +97,7 @@ print(f"Triangle 3 area (spherical) :{area_3_sph}")
 angle_3_sph = VectorSpherical.angle_vertices(v3a_sph, v3b_sph, v3c_sph)
 print(f"Triangle 3 angles (spherical):{angle_3_sph*180/np.pi}")
 
-#------------------------------
+# ------------------------------
 # TRIANGLE 4 - Spherical
 
 v4a_sph = VectorSpherical(1.0, np.pi/2, 0)
