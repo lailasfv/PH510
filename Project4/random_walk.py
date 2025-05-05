77f69fe14629d0e2f5e7862bfeab4655770daac6
lailasfv <sohaila.safavi.2019@uni.strath.ac.uk>
2025-05-05 23:25:58 +0100

    Changed function slightly, new idea noted

--- Project4/random_walk.py
+++ Project4/random_walk.py
@@ -18,24 +18,29 @@ from monte_carlo import MonteCarlo
 #      for that, but it is still coded into all the integration
 
 
-def random_walk(i, j, x, y):
+def random_walk(i, j, x, y, xn, yn):
     """
     Random walker starting at (i,j) in a grid from (0, 0) to (x, y)
-    This function should return the final position when the walker
-    reaches the grid edge
+    This function should return whether or not the walker reaches the
+    desired boundary point xn, yn.
     """
     pos = np.array([i,j])  # Starting position
+    pos2 = np.array([xn, yn]) # The position we hope to get to
     directions = np.array([[-1, 0], [1, 0], [0, -1], [0, 1]])  # Possible walks
 
     while pos[0]>0 and pos[0]<x and pos[1]>0 and pos[1]<y:  # While not at boundary
         move = directions(np.random.randint(0, len(directions))
         pos += move
 
-    return pos  # returns final position after it reaches boundary
+    return pos == pos2  # Returns 0 if not true and 1 if true
 
 
-# Next step - finding probability that i,j ends up at x_n, y_n
-#             for Green's function
+# I don't know if I like this approach yet
+# We'd basically have to have an array of all of the boundary positions (x, y)
+# And iterate N times through all of them? Which seems...bleh
 
-# I really have no idea how to do this rn
-# I think we might actually need to make a probability matrix? I'm not sure how yet
+# NOTE FOR TOMORROW: I can send in a whole array for xn yn
+# no more need to pass in x, y as it'll just be the max of [xn, yn]
+# and then when evaluating whether pos == pos2, it can add 1 to the boundary point
+# that it DID reach
+# I just need to check how this is handled logically in the code
